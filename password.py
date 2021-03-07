9f33575282dffea51a0c171b8376adbdfef2e885
Daisy <soniahsiao16@gmail..com>
2021-03-08 02:13:09 +0800

    better version

--- password.py
+++ password.py
@@ -2,12 +2,14 @@
 password = 'qqq123'
 i = 3  # 剩餘機會
 while i > 0:
+    i -= 1
     pw = input('請輸入密碼：')
     if pw == password:
         print('登入成功！')
         break
     else:
-        i -= 1
-        print('密碼錯誤！密碼錯誤！還有', i, '次機會')
-        # if i <= 0:
-        #     break
+        print('密碼錯誤!')
+        if i > 0:
+            print('密碼錯誤！還有', i, '次機會')
+        else:
+            print('很抱歉！您已多次嘗試失敗，請聯絡管理者')
